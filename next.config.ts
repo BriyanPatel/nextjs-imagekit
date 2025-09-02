65f9c7bfb19311d75714260dd5d3495c860e47a1
Briyan Hinrajiya <BL0147@bliss.local>
2025-09-02 17:01:59 +0530

    init

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,18 @@
 import "./src/env";
 
-import type {NextConfig} from "next";
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [{hostname: "ik.imagekit.io"}],
   },
+  eslint: {
+    dirs: ["src"],
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  }
 };
 
 export default nextConfig;
